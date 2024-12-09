a49eda90fd6611adddd0948d5c60a571a15429ea
Nikolay-Alikin <n1kolayalikin@yandex.ru>
2024-12-09 22:30:09 +0300

    произведена настройка spring cloud config server

--- core/ewm/src/main/java/com/github/artemlv/ewm/event/config/WebClientConfig.java
+++ core/ewm/src/main/java/com/github/artemlv/ewm/event/config/WebClientConfig.java
@@ -3,6 +3,7 @@ package com.github.artemlv.ewm.event.config;
 import com.github.artemlv.ewm.exception.type.ConflictException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,4 +38,5 @@ public class WebClientConfig {
                         .build()
         );
     }
+
 }

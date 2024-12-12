7c678ea3a3aa7689dd5ca7c60671f0a390b57f61
Nikolay-Alikin <n1kolayalikin@yandex.ru>
2024-12-12 14:33:14 +0300

    правки после 2 ревью по 1 тз

--- stats/stats-client/src/main/java/ru/yandex/practicum/client/StatsClientImpl.java
+++ stats/stats-client/src/main/java/ru/yandex/practicum/client/StatsClientImpl.java
@@ -1,11 +1,19 @@
 package ru.yandex.practicum.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.NotFoundException;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestClient;
@@ -20,18 +28,29 @@ public class StatsClientImpl implements StatsClient {
     private static final String HIT_PATH = "/hit";
     private static final String STATS_PATH = "/stats";
 
-    private final RestClient restClient;
-    private final DiscoveryService discoveryService;
+    private final DiscoveryClient discoveryClient;
+    private final RetryTemplate retryTemplate;
 
-    public StatsClientImpl(DiscoveryService discoveryService) {
-        this.discoveryService = discoveryService;
-        this.restClient = RestClient.builder()
-                .baseUrl(discoveryService.getServiceUrl("stats"))
-                .build();
+    @Value("${stats-service.id}")
+    private String statsServiceId;
+
+    public StatsClientImpl(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(retryPolicy);
+        this.retryTemplate = retryTemplate;
     }
 
     public List<StatCountHitsDto> get(final SearchStats searchStats) {
-        Object[] listObj = restClient.get()
+        Object[] listObj = RestClient.create()
+                .get()
                 .uri(uriBuilder -> uriBuilder.path(STATS_PATH)
                         .queryParam("start", searchStats.getStart())
                         .queryParam("end", searchStats.getEnd())
@@ -53,11 +72,30 @@ public class StatsClientImpl implements StatsClient {
 
     @Override
     public ResponseEntity<Void> save(final CreateStatsDto request) {
-        return restClient.post()
-                .uri(HIT_PATH)
+        return RestClient.create().post()
+                .uri(makeUri(HIT_PATH))
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(request)
                 .retrieve()
                 .toBodilessEntity();
+
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient
+                    .getInstances(statsServiceId)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new NotFoundException(
+                    "Ошибка обнаружения адреса сервиса статистики с id: " + statsServiceId,
+                    exception
+            );
+        }
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(cxt -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
     }
 }
